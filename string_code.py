534c818ffa1c2e902b3a264bd4db3054e1c8ea0b
yinghuangcheng <875812310@qq.com>
2018-02-10 15:00:07 +0800

    【add】提交第一次测试代码

---
+++ string_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print u'应煌程'  #中文必须是unicode编码
+
+print u'**************下面进行字符串格式化测试*************'
+strMsg = 'hello %s' % ('yinghuangcheng')
+print strMsg
+
+strMsg = u'hello %s' % (u'应煌程')
+print strMsg
